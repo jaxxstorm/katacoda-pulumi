13cc471594f0061e2fce3200a44d7911dea86afe
Lee Briggs <lee@leebriggs.co.uk>
2021-01-19 19:49:45 -0800

    wait for env to be configured

--- introduction-to-pulumi/first-resource-typescript/login.sh
+++ introduction-to-pulumi/first-resource-typescript/login.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-# reload terminal to ensure pulumi is in the path
+show_progress()
+{
+  echo -n "Starting"
+  local -r pid="${1}"
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do
+    if [[ -f "$HOME/.pulumi/bin" ]]; then  
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  echo "Pulumi installed"
+}
+
+show_progress
+# Reload the shell
 exec bash
